0cfe3fef2dd38bfee203a0220cbea3421fa28ba1
Robin Heidenis <robin.heidenis@gmail.com>
2024-03-01 02:54:14 +0000

    fix: 🐛 build error by wrapping searchform in suspense
    thanks react server components

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,14 @@
 import { SearchForm } from "~/components/SearchForm";
+import { Suspense } from "react";
 
 export default function SearchPage() {
   return (
     <main className="mx-auto w-3/4 p-4 text-center">
       <h1 className="text-6xl text-peach">Search</h1>
       <h2 className={"mt-5 text-2xl text-peach"}>Search movies and shows</h2>
-      <SearchForm />
+      <Suspense>
+        <SearchForm />
+      </Suspense>
     </main>
   );
 }
